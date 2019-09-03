89bb6af6ee415c9e32e8d059ea8e3a35486610a6
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-03 17:35:56 -0500

    Taller de edades e iguales

---
+++ taller3.3/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace taller3._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.lblmayor = new System.Windows.Forms.Label();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.lblmenor = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identificador de Mayor y Menor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el Primer Número:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese el Segundo Número:";
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum1.Location = new System.Drawing.Point(416, 127);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 32);
+            this.txtnum1.TabIndex = 3;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum2.Location = new System.Drawing.Point(416, 185);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 32);
+            this.txtnum2.TabIndex = 4;
+            // 
+            // lblmayor
+            // 
+            this.lblmayor.AutoSize = true;
+            this.lblmayor.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmayor.Location = new System.Drawing.Point(282, 316);
+            this.lblmayor.Name = "lblmayor";
+            this.lblmayor.Size = new System.Drawing.Size(75, 24);
+            this.lblmayor.TabIndex = 5;
+            this.lblmayor.Text = "Mayor ";
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenviar.Location = new System.Drawing.Point(566, 160);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(93, 46);
+            this.btnenviar.TabIndex = 6;
+            this.btnenviar.Text = "Enviar";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // lblmenor
+            // 
+            this.lblmenor.AutoSize = true;
+            this.lblmenor.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmenor.Location = new System.Drawing.Point(607, 316);
+            this.lblmenor.Name = "lblmenor";
+            this.lblmenor.Size = new System.Drawing.Size(75, 24);
+            this.lblmenor.TabIndex = 7;
+            this.lblmenor.Text = "Menor ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(70, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "El Número Mayor es:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(395, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "El Número Menor es:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblmenor);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.lblmayor);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Label lblmayor;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label lblmenor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ taller3.3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taller3._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = int.Parse(txtnum1.Text);
+            num2 = int.Parse(txtnum2.Text);
+
+            if (num1 > num2)
+            {
+                lblmayor.Text = num1.ToString();
+                lblmenor.Text = num2.ToString();
+
+                lblmayor.Visible = true;
+                lblmenor.Visible = true;
+
+            }
+            else
+            if (num2>num1)
+            {
+
+                lblmayor.Text = num2.ToString();
+                lblmenor.Text = num1.ToString();
+
+                lblmayor.Visible = true;
+                lblmenor.Visible = true;
+
+            }
+
+            else 
+                if (num1 == num2)
+            {
+                MessageBox.Show("Ambos Números son iguales");
+                lblmayor.Visible = false;
+                lblmenor.Visible = false;
+
+            }
+        }
+
+    }
+}
